00b8d2dcf32c86c7a0a4b39fd2817b542bf75808
bharathgujjari <bharath17.gujjari@gmail.com>
2019-05-21 15:34:11 +0530

    files uploaded

---
+++ src/main/java/infobyt/App.java
@@ -0,0 +1,13 @@
+package infobyt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
